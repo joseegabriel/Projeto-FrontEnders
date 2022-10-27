0e3693897f85f66bfeb0631b0babc2fe10047682
unknown <joseg081@gmail.com>
2022-10-27 16:11:07 -0300

    Footer updates

--- src/js/script.js
+++ src/js/script.js
@@ -15,3 +15,15 @@ function toggleMenu(event) {
 
 btnMobile.addEventListener('click', toggleMenu);
 btnMobile.addEventListener('touchstart', toggleMenu);
+
+let input = document.querySelector(".input");
+let button = document.querySelector(".button");
+button.disabled = true;
+input.addEventListener("change", stateHandle);
+function stateHandle() {
+  if (document.querySelector(".input").value === "") {
+    button.disabled = true; 
+  } else {
+    button.disabled = false;
+  }
+}
